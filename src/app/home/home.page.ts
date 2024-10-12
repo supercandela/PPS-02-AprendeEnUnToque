04aa839db0e1cc93ce20efc2e2dc462e63b10a0f
supercandela <supercandela@gmail.com>
2024-10-12 19:20:35 -0300

    pantalla de idiomas funcionando con audios en click

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,7 +9,8 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  temaSeleccionado: string = '';
+  temaSeleccionado: string = 'colores';
+  idiomaSeleccionado: string = 'espanol';
   // Rutas de las imágenes para cada tema
   imagenesPorTema: { [key: string]: string[] } = {
     colores: [
@@ -40,9 +41,7 @@ export class HomePage implements OnInit {
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit() {
-    this.temaSeleccionado = 'colores';
-  }
+  ngOnInit() {}
 
   // Método para seleccionar un tema
   onSeleccionarTema(tema: string) {
@@ -51,13 +50,100 @@ export class HomePage implements OnInit {
 
   // Método para obtener la ruta de la imagen según el botón
   obtenerRutaImagen(boton: number): string {
-    console.log(this.imagenesPorTema[this.temaSeleccionado][boton - 1]);
     return this.imagenesPorTema[this.temaSeleccionado][boton - 1];
   }
 
-  onSeleccionarIdioma(idioma: number) {}
+  onSeleccionarIdioma(idioma: string) {
+    this.idiomaSeleccionado = idioma;
+  }
 
-  onMainButtonClick(num: number) {}
+  onReproducirSonido(botonId: number) {
+    this.reproducirSonido(botonId);
+  }
+
+  // Método para reproducir sonido
+  reproducirSonido(botonId: number) {
+    const rutaSonido = this.obtenerRutaSonido(botonId);
+    const audio = new Audio(rutaSonido);
+    audio.play();
+  }
+
+  obtenerRutaSonido(botonId: number) {
+    let archivoSonido = '';
+
+    switch (this.temaSeleccionado) {
+      case 'colores':
+        switch (botonId) {
+          case 1:
+            archivoSonido = 'rojo';
+            break;
+          case 2:
+            archivoSonido = 'naranja';
+            break;
+          case 3:
+            archivoSonido = 'amarillo';
+            break;
+          case 4:
+            archivoSonido = 'verde';
+            break;
+          case 5:
+            archivoSonido = 'azul';
+            break;
+          case 6:
+            archivoSonido = 'violeta';
+            break;
+        }
+        break;
+
+      case 'numeros':
+        switch (botonId) {
+          case 1:
+            archivoSonido = '1';
+            break;
+          case 2:
+            archivoSonido = '2';
+            break;
+          case 3:
+            archivoSonido = '3';
+            break;
+          case 4:
+            archivoSonido = '4';
+            break;
+          case 5:
+            archivoSonido = '5';
+            break;
+          case 6:
+            archivoSonido = '6';
+            break;
+        }
+        break;
+
+      case 'animales':
+        switch (botonId) {
+          case 1:
+            archivoSonido = 'perro';
+            break;
+          case 2:
+            archivoSonido = 'gato';
+            break;
+          case 3:
+            archivoSonido = 'mono';
+            break;
+          case 4:
+            archivoSonido = 'conejo';
+            break;
+          case 5:
+            archivoSonido = 'caballo';
+            break;
+          case 6:
+            archivoSonido = 'loro';
+            break;
+        }
+        break;
+    }
+
+    return `'../../assets/sounds/${this.idiomaSeleccionado}-${this.temaSeleccionado}-${archivoSonido}.ogg`;
+  }
 
   onLogoutClick() {
     this.authService.logout();
